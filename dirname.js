b661f28de1bef6b74c19bcba8ac9f3fc452ace6c
Tiger Oakes <tigeroakes@gmail.com>
2016-08-16 09:01:46 -0700

    Moved __dirname to a file in the folder root to ensure that it outputs correctly after building

---
+++ dirname.js
@@ -0,0 +1,2 @@
+//Ensures the dirname is located in the root of the folder
+export default __dirname;
--- js/paths.js
+++ js/paths.js
@@ -1,7 +1,8 @@
 import {resolve} from 'path';
+import dirname from '../dirname.js';
 
 /** @type {string} Path to the partials folder */
-export const partials = resolve(__dirname, '../partials');
+export const partials = resolve(dirname, '../partials');
 
 /** @type {string} Path to the helpers folder */
-export const helpers = resolve(__dirname, '../helpers');
+export const helpers = resolve(dirname, '../helpers');
